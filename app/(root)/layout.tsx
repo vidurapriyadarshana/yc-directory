f4c4891b82ab9143f41299cebe7653362eb3fe8f
Vidura Priyadarshana <vidura200427@gmail.com>
2025-05-04 16:51:29 +0530

    Initial commit

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const layout = ({ children }: Readonly<{ children: React.ReactNode }>) => {
+    return (
+        <main>
+            {children}
+        </main>
+    )
+}
+
+export default layout
