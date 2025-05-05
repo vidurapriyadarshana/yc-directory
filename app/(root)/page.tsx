1aec6d490aba63d5b81987e82edaabaed6317bcd
Vidura Priyadarshana <vidura200427@gmail.com>
2025-05-05 12:24:08 +0530

    Hero Section Complete

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,8 +3,18 @@ import React from 'react'
 const page = () => {
   return (
     <>
-     <h1 className='text-2xl'>Home</h1>
-     
+      <section className='bg-pink-500 w-full bg-primary min-h-[530px] flex justify-center items-center flex-col py-10 px-6' >
+        <h1 className='uppercase bg-black px-6 py-3 font-work-sans font-extrabold text-white sm:text-[54px] sm:leading-[64px] text-[36px] leading-[46px] max-w-5xl text-center my-5'>
+          Pitch your startup, connect with enterpreneurs
+        </h1>
+
+        <p className='font-bold text-[20px] text-white text-center break-words !max-w-3xl'>
+          Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions.
+        </p>
+
+      </section>
+
+
     </>
   )
 }
